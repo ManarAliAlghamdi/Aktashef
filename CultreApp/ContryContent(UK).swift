b82f8b4d1637d4ec235db6904d03537df1a453d6
Faialharthi <faialialharthi.d@gmail.com>
2024-09-30 20:59:36 +0300

    Country Content Page

---
+++ CultreApp/ContryContent(UK).swift
@@ -0,0 +1,18 @@
+//
+//  ContryContent(UK).swift
+//  CultreApp
+//
+//  Created by Fai Alharthi on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContryContent_UK_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContryContent_UK_()
+}
