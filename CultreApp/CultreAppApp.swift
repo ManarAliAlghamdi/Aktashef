c655a311198c3bea4071f9092667154d084d2707
Manar Alghamdi <manar.a.gh@hotmail.com>
2024-09-30 14:49:51 +0300

    Initial Commit

---
+++ CultreApp/CultreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CultreAppApp.swift
+//  CultreApp
+//
+//  Created by Manar Alghmadi on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct CultreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
