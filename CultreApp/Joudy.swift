65653a2a96fee6ca5b5f4d17a232dbb2dbdb0dd9
joudy003 <joudy.j.t@gmail.com>
2024-09-30 16:28:41 +0300

    testing

--- CultreApp/Joudy.swift
+++ CultreApp/Joudy.swift
@@ -1,17 +1,36 @@
-//
-//  Joudy.swift
-//  CultreApp
-//
-//  Created by Joud Abdullah AlAmri on 9/30/24.
-//
-
 import SwiftUI
 
 struct Joudy: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        NavigationView {
+        VStack {
+        Spacer()}.navigationBarItems(leading:
+ HStack {
+ Button(action: {
+     // Action for list icon
+     print("List icon tapped")}) {
+    Image(systemName: "line.horizontal.3")
+    .imageScale(.large)
+    .foregroundColor(.black) // Change color if needed
+     }
+
+     // Adding space between icons
+     Spacer().frame(width: 16)
+         Button(action: {
+          // Action for grid icon
+       print("Grid icon tapped")})
+            {Image(systemName: "square.grid.2x2").imageScale(.large)
+                    .foregroundColor(.black) // Change color if needed
+            }
+                       }
+    , trailing:
+    Button(action: {
+  // Action for favorite button
+    print("Favorite button tapped")}) {
+    Image(systemName: "heart").imageScale(.large).foregroundColor(.black)})}
+        
+    }// End of Body
+}// End of View
 
 #Preview {
     Joudy()
