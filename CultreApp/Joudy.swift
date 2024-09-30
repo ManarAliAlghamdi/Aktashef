bf8ec6be8f3c6a53c86b995ab525028d88d84649
joudy003 <joudy.j.t@gmail.com>
2024-09-30 16:19:55 +0300

    123

---
+++ CultreApp/Joudy.swift
@@ -0,0 +1,18 @@
+//
+//  Joudy.swift
+//  CultreApp
+//
+//  Created by Joud Abdullah AlAmri on 9/30/24.
+//
+
+import SwiftUI
+
+struct Joudy: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Joudy()
+}
