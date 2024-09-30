233414af494999d55832d0860cac063b9853ab6b
Albatoul <ebatoul25@gmail.com>
2024-09-30 17:25:57 +0300

    30Sep5:25

---
+++ CultreApp/favesCountryGrid.swift
@@ -0,0 +1,18 @@
+//
+//  favesCountryGrid.swift
+//  CultreApp
+//
+//  Created by Albatoul Alhamas on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct favesCountryGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    favesCountryGrid()
+}
