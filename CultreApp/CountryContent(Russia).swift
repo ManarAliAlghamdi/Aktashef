97eed2c12e815c139d7725b840cda7c651462ef0
Faialharthi <faialialharthi.d@gmail.com>
2024-10-06 15:23:04 +0300

    ContryContent_UK page 6 october , 3:22pm

---
+++ CultreApp/CountryContent(Russia).swift
@@ -0,0 +1,18 @@
+//
+//  CountryContent(Russia).swift
+//  CultreApp
+//
+//  Created by Fai Alharthi on 03/04/1446 AH.
+//
+
+import SwiftUI
+
+struct CountryContent_Russia_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountryContent_Russia_()
+}
